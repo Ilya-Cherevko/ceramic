45450cea3df0deddbe1b1bdde540679b7be3d78b
Ilya-Cherevko <ya.titanikum@yandex.ru>
2024-09-12 15:53:50 +0400

    trening next

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Fcomponent name="Ilya"/>
-        <Ccomponent namber="345"/>
+        
+        <Ccomponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react'
 import Menu from './menu'
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 export default class Ccomponent extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       /*visibility: false*/
+       count: 0
+
+    }
+
+    /*this.handleClick=this.handleClick.bind(this);*/
+    this.increment=this.increment.bind(this);
+    this.decrement=this.decrement.bind(this);
+    this.reset=this.reset.bind(this);
+  }
+  
+  /*handleClick() {
+    this.setState(state => ({
+      visibility: !state.visibility
+    }));
+  }*/
+
+    increment() {
+      this.setState(state => ({
+        count: state.count +1
+      }));
+    }
+
+    decrement() {
+      this.setState(state => ({
+        count: state.count -1
+      }));
+    }
+
+    reset() {
+      this.setState({count: 0});
+    }
+
   render() {
-    return (
+    return(
       <div>
-        <Menu />
-        <h1>Class component {this.props.namber}</h1>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.reset}>reset</button>
+        <h1>Current: {this.state.count}</h1>
       </div>
     )
+    /*if (this.state.visibility) {
+
+       return (
+      <div>
+        
+        <h1>Now you see my</h1>
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    );
+  } else {
+      return <div>
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    }*/
   }
 }
