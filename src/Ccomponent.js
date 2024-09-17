3501d6f2602452cd4a3f4dd7d9a7b2a6c6ead786
Ilya-Cherevko <ya.titanikum@yandex.ru>
2024-09-17 17:14:53 +0400

    you

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Menu from './menu';
-import { isVisible } from '@testing-library/user-event/dist/utils';
+/*import Menu from './menu';
+import { isVisible } from '@testing-library/user-event/dist/utils';*/
 /*import Card from '../src/Constants/cards';
 import img from '../src/images/catalog/'*/
 
@@ -10,23 +10,39 @@ export default class Ccomponent extends Component {
   
     this.state = {
        /*visibility: false*/
-       count: 0
+       /*count: 0*/
+      input: '',
+      submit: ''
+       };
 
-    }
+       this.handleChange=this.handleChange.bind(this);
+       this.handleSubmit=this.handleSubmit.bind(this);
 
     /*this.handleClick=this.handleClick.bind(this);*/
-    this.increment=this.increment.bind(this);
+    /*this.increment=this.increment.bind(this);
     this.decrement=this.decrement.bind(this);
-    this.reset=this.reset.bind(this);
+    this.reset=this.reset.bind(this);*/
   }
   
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({
+      input: this.state.input
+    });
+  }
   /*handleClick() {
     this.setState(state => ({
       visibility: !state.visibility
     }));
   }*/
 
-    increment() {
+  /*  increment() {
       this.setState(state => ({
         count: state.count +1
       }));
@@ -40,16 +56,23 @@ export default class Ccomponent extends Component {
 
     reset() {
       this.setState({count: 0});
-    }
+    }*/
 
   render() {
     return(
-      <div>
+   /*   <div>
         <button onClick={this.increment}>increment</button>
         <button onClick={this.decrement}>decrement</button>
         <button onClick={this.reset}>reset</button>
         <h1>Current: {this.state.count}</h1>
         
+      </div>*/
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.input} onChange={this.handleChange}/>
+          <button type='submit'>Submit!</button>
+        </form>
+        <h3>{this.state.submit}</h3>
       </div>
     )
     /*if (this.state.visibility) {
