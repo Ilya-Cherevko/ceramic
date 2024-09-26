2aac7759cb077a9d5832ad22fd33db718d384400
Ilya-Cherevko <ya.titanikum@yandex.ru>
2024-09-26 22:15:01 +0400

    фильтрация карточек

---
+++ src/Cards.js
@@ -0,0 +1,71 @@
+const Cards = [
+    {
+    id: '0',
+    Kantry: 'italy',
+    Name: 'delacora',
+    Kollection: 'arts',
+    Tip: 'keramogranit',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x500.jpg'),
+      require('./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x300.jpg')
+    ], 
+    }, {
+    id: '1',
+    Kantry: 'italy',
+    Name: 'graniteya',
+    Kollection: 'iremel',
+    Tip: 'keramogranit',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg'),
+      require('./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg')
+    ],
+    }, {
+    id: '2',
+    Kantry: 'india',
+    Name: 'absolut_gres',
+    Kollection: 'agate',
+    Tip: 'keramogranit',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+    ],
+    }, {
+    id: '3',
+    Kantry: 'india',
+    Name: 'absolut_gres',
+    Kollection: 'agate',
+    Tip: 'plitka',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+    ],
+    }, {
+    id: '4',
+    Kantry: 'india',
+    Name: 'absolut_gres',
+    Kollection: 'agate',
+    Tip: 'plitka',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+    ],
+    }, {
+    id: '5',
+    Kantry: 'india',
+    Name: 'absolut_gres',
+    Kollection: 'agate',
+    Tip: 'plitka',
+    Size: '60x120',
+    interiors: [
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
+      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+    ],
+    }
+];
+
+export default Cards
--- src/Keramogranit.js
+++ src/Keramogranit.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import './card__wrapper.css'
 import './Card.css'
+import Cards from './Cards'
 
 export default function Keramogranit () {
-  
+  const keramogranit = Cards.filter(card =>
+    card.Tip === 'keramogranit'
+  );
   return (
-    <div className="card__wrapper">
-        {Cards.map((card, id) => (
-          <div className="card__body">
-              <img className="card__img" src={card.interiors} alt={`${card.Kollection + id + card.Name}`} />
+  <ul className="card__wrapper">
+  {keramogranit.map(card =>
+    <li key={card.id}>
+        <div className="card__body">
+              <img className="card__img" src={card.interiors[0]} alt={`${card.Kollection + card.Name}`} />
               <p className="card__kollection">{card.Kollection}</p>
               <p className="card__name">{card.Name}</p>
               <p className="card__kantry">{card.Kantry}</p>
               <p className="card__size">{card.Size}</p>
-          </div>
-        ))}
-      </div>
-    )
+        </div>
+    </li>
+     
+  )}
+  </ul>
+  )  
 }
-
-export const Cards = [
-    {
-    id: '1',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Size: '60x120',
-    interiors: require('../src/images/catalog/azori/acate/interiors/azori_acate_17906_167402_0x300.jpg'),
-    },
-    {
-        id: '2',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Size: '60x120',
-    interiors: require('../src/images/catalog/azori/acate/interiors/azori_acate_17906_300786_0x300.jpg'),
-    },
-    {
-        id: '3',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Size: '60x120',
-    interiors: require('../src/images/catalog/azori/acate/interiors/azori_acate_17906_309981_0x300.jpg'),
-    }
-]
