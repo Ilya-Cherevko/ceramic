550f29f448cf0418e1ad6cba43ca7b73af3c1968
Ilya-Cherevko <ya.titanikum@yandex.ru>
2024-09-30 23:36:02 +0400

    baza otladka

--- src/Cards.js
+++ src/Cards.js
@@ -1,71 +1,94 @@
 const Cards = [
-    {
-    id: '0',
-    Kantry: 'italy',
-    Name: 'delacora',
-    Kollection: 'arts',
-    Tip: 'keramogranit',
-    Size: '60x120',
+  {
+    id: "0",
+    Kantry: "italy",
+    Name: "delacora",
+    Kollection: "arts",
+    Tip: "keramogranit",
+    Size: "60x120",
     interiors: [
-      require('./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x500.jpg'),
-      require('./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x300.jpg')
-    ], 
-    }, {
-    id: '1',
-    Kantry: 'italy',
-    Name: 'graniteya',
-    Kollection: 'iremel',
-    Tip: 'keramogranit',
-    Size: '60x120',
-    interiors: [
-      require('./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg'),
-      require('./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg')
+      require("./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x500.jpg"),
+      require("./images/catalog/delacora/arts/interiors/delacora_arts_14007_559809_0x300.jpg"),
     ],
-    }, {
-    id: '2',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Tip: 'keramogranit',
-    Size: '60x120',
+  },
+  {
+    id: "1",
+    Kantry: "italy",
+    Name: "graniteya",
+    Kollection: "iremel",
+    Tip: "keramogranit",
+    Size: "60x120",
     interiors: [
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+      require("./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg"),
+      require("./images/catalog/graniteya/iremel/interiors/graniteya_iremel_9203_144974_0x500.jpg"),
     ],
-    }, {
-    id: '3',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Tip: 'plitka',
-    Size: '60x120',
+  },
+  {
+    id: "2",
+    Kantry: "india",
+    Name: "absolut_gres",
+    Kollection: "agate",
+    Tip: "keramogranit",
+    Size: "60x120",
     interiors: [
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
     ],
-    }, {
-    id: '4',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Tip: 'plitka',
-    Size: '60x120',
+  },
+  {
+    id: "3",
+    Kantry: "india",
+    Name: "absolut_gres",
+    Kollection: "agate",
+    Tip: "plitka",
+    Size: "60x120",
     interiors: [
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
     ],
-    }, {
-    id: '5',
-    Kantry: 'india',
-    Name: 'absolut_gres',
-    Kollection: 'agate',
-    Tip: 'plitka',
-    Size: '60x120',
+  },
+  {
+    id: "4",
+    Kantry: "india",
+    Name: "absolut_gres",
+    Kollection: "agate",
+    Tip: "plitka",
+    Size: "60x120",
     interiors: [
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg'),
-      require('./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg')
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
     ],
-    }
+  },
+  {
+    id: "5",
+    Kantry: "india",
+    Name: "absolut_gres",
+    Kollection: "agate",
+    Tip: "plitka",
+    Size: "60x120",
+    interiors: [
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
+      require("./images/catalog/azori/acate/interiors/azori_acate_17906_467540_0x500.jpg"),
+    ],
+  },
+  {
+    id: "6",
+    Kantry: "Индия",
+    Name: "absolut_gres",
+    Kollection: "boston_light",
+    Tip: "keramogranit",
+    Size: "60x120",
+    interiors: [
+      require("./images/catalog/absolut_gres/boston_light/interiors/absolut_gres_boston_light_17724_752067_0x500.jpg"),
+      require("./images/catalog/absolut_gres/boston_light/interiors/absolut_gres_boston_light_17724_260803_0x300.jpg"),
+      require("./images/catalog/absolut_gres/boston_light/interiors/absolut_gres_boston_light_17724_520011_0x300.jpg"),
+      require("./images/catalog/absolut_gres/boston_light/interiors/absolut_gres_boston_light_17724_641625_0x300.jpg"),
+      require("./images/catalog/absolut_gres/boston_light/interiors/absolut_gres_boston_light_17724_752067_0x300.jpg"),
+    ],
+    tovars: [
+      require("./images/catalog/absolut_gres/boston_light/tovars/absolut_gres_boston_light_124842_913845_300x0.jpg"),
+    ],
+  },
 ];
 
-export default Cards
+export default Cards;
--- src/Keramogranit.js
+++ src/Keramogranit.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import './card__wrapper.css'
-import './Card.css'
-import Cards from './Cards'
+import React from "react";
+import "./card__wrapper.css";
+import "./Card.css";
+import Cards from "./Cards";
 
-export default function Keramogranit () {
-  const keramogranit = Cards.filter(card =>
-    card.Tip === 'keramogranit'
-  );
+export default function Keramogranit() {
+  const keramogranit = Cards.filter((card) => card.Tip === "keramogranit");
+  console.log(keramogranit);
   return (
-  <ul className="card__wrapper">
-  {keramogranit.map(card =>
-    <li key={card.id}>
-        <div className="card__body">
-              <img className="card__img" src={card.interiors[0]} alt={`${card.Kollection + card.Name}`} />
-              <p className="card__kollection">{card.Kollection}</p>
-              <p className="card__name">{card.Name}</p>
-              <p className="card__kantry">{card.Kantry}</p>
-              <p className="card__size">{card.Size}</p>
-        </div>
-    </li>
-     
-  )}
-  </ul>
-  )  
+    <ul className="card__wrapper">
+      {keramogranit.map((card) => (
+        <li key={card.id}>
+          <div className="card__body">
+            <img
+              className="card__img"
+              src={card.interiors[0]}
+              alt={`${card.Kollection + card.Name}`}
+            />
+            <p className="card__kollection">{card.Kollection}</p>
+            <p className="card__name">{card.Name}</p>
+            <p className="card__kantry">{card.Kantry}</p>
+            <p className="card__size">{card.Size}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
