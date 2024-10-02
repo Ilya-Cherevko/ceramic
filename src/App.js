719c0dae7e8f217f5f7eebe4452f6f94014dd55f
Ilya-Cherevko <ya.titanikum@yandex.ru>
2024-10-02 22:35:14 +0400

    baza otladka3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import './App.css';
-import Header from './Header';
-import { Routes, Route } from 'react-router-dom';
-import Plitka from './Plitka';
-import Keramogranit from './Keramogranit'
-import NavBar from './NavBar';
-import Azori from './Azori';
-import Estima from './Estima';
-import Content from '../src/Components/Slider/Content/Content'
-import Ffooter from './Ffooter';
-import AboutUs from './AboutUs';
+import "./App.css";
+import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
+import Plitka from "./Plitka";
+import Keramogranit from "./Keramogranit";
+import Azori from "./Azori";
+import Estima from "./Estima";
+import Content from "../src/Components/Slider/Content/Content";
+import Ffooter from "./footer";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
       <Header />
-      <NavBar />
-        <Routes>
-          <Route path="/"  element={<Content />}/>
-          <Route path="/Plitka" element={<Plitka />} />
-            <Route path="Azori" element={<Azori />} />
-          <Route path="/Keramogranit" element={<Keramogranit />} />
-           <Route path="Estima" element={<Estima />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-        </Routes>
-      <Ffooter/>
+      <Routes>
+        <Route path="/" element={<Content />} />
+        <Route path="/Plitka" element={<Plitka />} />
+        <Route path="Azori" element={<Azori />} />
+        <Route path="/Keramogranit" element={<Keramogranit />} />
+        <Route path="Estima" element={<Estima />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+      </Routes>
+      <Ffooter />
     </div>
-    
   );
 }
 
